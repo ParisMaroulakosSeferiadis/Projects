78cbf8e7a65c431111f4412f03439ad360130511
Maroulakos-Seferiadis Paris <98891748+ParisMaroulakosSeferiadis@users.noreply.github.com>
2022-02-02 20:06:36 +0200

    Delete meteorological-station-network directory

--- meteorological-station-network/server-web-application-code/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = "paris";
-$pass = "paris1997";
-$host = "localhost:3306";
-$db_name = "ethernet";
-$con = mysqli_connect ($host, $username, $pass);
-$db = mysqli_select_db ( $con, $db_name );
-?>
